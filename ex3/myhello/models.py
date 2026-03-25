15fa05b7e727fb02983c303d4c2766d1db4c041d
lin-jessic <B1229062@cgu.edu.tw>
2026-03-25 14:19:16 +0800

    delete

--- ex3/myhello/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.db import models
-
-class Course_table(models.Model):
-    Department = models.CharField(max_length=100)
-    CourseTitle = models.CharField(max_length=200)
-    Instructor = models.CharField(max_length=100)
--- ex3/myhello/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('addcourse', views.add_course, name='add_course'),
-    path('courselist', views.list_course, name='list_course'),
-]
